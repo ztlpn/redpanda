3f15569f3f2055fc552d167501d8cfc9690503cc
Alexey Zatelepin <ztlpn@redpanda.com>
2023-08-11 16:57:59 +0200

    concurrency limit on votes

--- src/v/raft/consensus.cc
+++ src/v/raft/consensus.cc
@@ -53,6 +53,19 @@
 #include <iterator>
 #include <system_error>
 
+class raft_resources {
+public:
+    ss::semaphore vote{128};
+
+    raft_resources()
+      : vote(config::shard_local_cfg().raft_concurrent_votes_per_shard()) {}
+};
+
+raft_resources& get_raft_resources() {
+    static thread_local raft_resources r;
+    return r;
+}
+
 template<>
 struct fmt::formatter<raft::consensus::vote_state> final
   : fmt::formatter<std::string_view> {
@@ -882,6 +895,21 @@ void consensus::dispatch_vote(bool leadership_transfer) {
         arm_vote_timeout();
         return;
     }
+
+    if (get_raft_resources().vote.current() <= 0) {
+        // Important to drop out before adjusting priority: this is not
+        // a real failure to vote, just a resource throttle.
+        vlog(
+          _ctxlog.info, "resources.vote: Too many votes in flight, deferring");
+        arm_vote_timeout();
+        return;
+    } else {
+        vlog(
+          _ctxlog.debug,
+          "resources.vote: Voting, sem count {}",
+          get_raft_resources().vote.current());
+    }
+
     auto self_priority = get_node_priority(_self);
     // check if current node priority is high enough
     // update target priority
@@ -921,50 +949,60 @@ void consensus::dispatch_vote(bool leadership_transfer) {
           self_priority,
           cur_target_priority);
     }
+
+    auto vote_units = ss::consume_units(get_raft_resources().vote, 1);
+
     // background, acquire lock, transition state
     ssx::background
-      = ssx::spawn_with_gate_then(_bg, [this, leadership_transfer] {
-            return dispatch_prevote(leadership_transfer)
-              .then([this, leadership_transfer](bool ready) mutable {
-                  if (!ready) {
-                      return ss::make_ready_future<>();
-                  }
-                  auto vstm = std::make_unique<vote_stm>(this);
-                  auto p = vstm.get();
-
-                  // CRITICAL: vote performs locking on behalf of consensus
-                  return p->vote(leadership_transfer)
-                    .then_wrapped([this, p, vstm = std::move(vstm)](
-                                    ss::future<> vote_f) mutable {
-                        try {
-                            vote_f.get();
-                        } catch (const ss::gate_closed_exception&) {
-                            // Shutting down, don't log.
-                        } catch (...) {
-                            vlog(
-                              _ctxlog.warn,
-                              "Error returned from voting process {}",
-                              std::current_exception());
-                        }
-                        auto f = p->wait().finally([vstm = std::move(vstm)] {});
-                        // make sure we wait for all futures when gate is closed
-                        if (_bg.is_closed()) {
-                            return f;
-                        }
-                        // background
-                        ssx::spawn_with_gate(
-                          _bg,
-                          [vstm = std::move(vstm), f = std::move(f)]() mutable {
-                              return std::move(f);
-                          });
-
+      = ssx::spawn_with_gate_then(
+          _bg,
+          [this, leadership_transfer] {
+              return dispatch_prevote(leadership_transfer)
+                .then([this, leadership_transfer](bool ready) mutable {
+                    if (!ready) {
                         return ss::make_ready_future<>();
-                    });
-              })
-              .finally([this] { arm_vote_timeout(); });
-        }).handle_exception([this](const std::exception_ptr& e) {
-            vlog(_ctxlog.warn, "Exception thrown while voting - {}", e);
-        });
+                    }
+                    auto vstm = std::make_unique<vote_stm>(this);
+                    auto p = vstm.get();
+
+                    // CRITICAL: vote performs locking on behalf of consensus
+                    return p->vote(leadership_transfer)
+                      .then_wrapped([this, p, vstm = std::move(vstm)](
+                                      ss::future<> vote_f) mutable {
+                          try {
+                              vote_f.get();
+                          } catch (const ss::gate_closed_exception&) {
+                              // Shutting down, don't log.
+                          } catch (...) {
+                              vlog(
+                                _ctxlog.warn,
+                                "Error returned from voting process {}",
+                                std::current_exception());
+                          }
+                          auto f = p->wait().finally(
+                            [vstm = std::move(vstm)] {});
+                          // make sure we wait for all futures when gate is
+                          // closed
+                          if (_bg.is_closed()) {
+                              return f;
+                          }
+                          // background
+                          ssx::spawn_with_gate(
+                            _bg,
+                            [vstm = std::move(vstm),
+                             f = std::move(f)]() mutable {
+                                return std::move(f);
+                            });
+
+                          return ss::make_ready_future<>();
+                      });
+                })
+                .finally([this] { arm_vote_timeout(); });
+          })
+          .handle_exception([this](const std::exception_ptr& e) {
+              vlog(_ctxlog.warn, "Exception thrown while voting - {}", e);
+          })
+          .finally([vu = std::move(vote_units)]() {});
 }
 
 void consensus::arm_vote_timeout() {
